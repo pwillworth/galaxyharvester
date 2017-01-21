a8d87d0ed797fbd3fff3b5d06829a7c312c8fb53
Paul Willworth <ioscode@gmail.com>
2017-01-21 12:09:59 -0800

    Fix Jakku resource types specific planet id and min max on geothermal to fix display issue.

--- database/updateIncremental/dbUpdate20170121.sql
+++ database/updateIncremental/dbUpdate20170121.sql
@@ -1,2 +1,4 @@
 use swgresource;
 ALTER TABLE tUsers ADD COLUMN emailChange VARCHAR(255);
+UPDATE tResourceType SET PEmax=1000, OQmin=1, OQmax=1000 WHERE resourceType LIKE '%geothermal%';
+UPDATE tResourceType SET specificPlanet=18 WHERE resourceType LIKE '%jakk%';
