722d4fe4fe80280aaf7e6027c37576009c2714c9
Paul Willworth <ioscode@gmail.com>
2017-02-05 20:16:29 -0800

    Add columns to Creature and Schematic tables to allow manual entries by users.

---
+++ database/updateIncremental/dbUpdate20170205.sql
@@ -0,0 +1,6 @@
+use swgresource;
+ALTER TABLE tResourceTypeCreature ADD COLUMN missionLevel SMALLINT;
+ALTER TABLE tResourceTypeCreature ADD COLUMN galaxy SMALLINT DEFAULT 0 NOT NULL;
+ALTER TABLE tResourceTypeCreature ADD COLUMN enteredBy VARCHAR(31);
+ALTER TABLE tSchematic ADD COLUMN galaxy SMALLINT DEFAULT 0 NOT NULL;
+ALTER TABLE tSchematic ADD COLUMN enteredBy VARCHAR(31);
