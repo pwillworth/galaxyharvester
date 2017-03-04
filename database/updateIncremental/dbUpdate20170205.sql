a2baaeea835f611fd20dc55427cca39ec56dcb4b
Paul Willworth <ioscode@gmail.com>
2017-03-04 11:25:33 -0800

    Fix dbupdate script bugs

--- database/updateIncremental/dbUpdate20170205.sql
+++ database/updateIncremental/dbUpdate20170205.sql
@@ -19,7 +19,7 @@ ALTER TABLE tSkillGroup MODIFY COLUMN skillGroup VARCHAR(31) PRIMARY KEY;
 ALTER TABLE tProfession ADD COLUMN galaxy INT DEFAULT 0 NOT NULL;
 LOAD DATA LOCAL INFILE '/var/www/database/seedData/professions_32.csv' INTO TABLE tProfession FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' (profID, profName, craftingQuality, galaxy);
 LOAD DATA LOCAL INFILE '/var/www/database/seedData/tSkillGroup_32.txt' INTO TABLE tSkillGroup;
-ALTER TABLE tSchematicEvents DROP PRIMARY KEY (spawnID, schematicID, expGroup);
+ALTER TABLE tSchematicEvents DROP PRIMARY KEY;
 ALTER TABLE tSchematicEvents DROP INDEX spawnID;
 ALTER TABLE tSchematicEvents ADD INDEX IX_schemevt_spawnschemexpgrp (spawnID, schematicID, expGroup);
 LOAD DATA LOCAL INFILE '/var/www/database/seedData/professions_48.csv' INTO TABLE tProfession FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' (profID, profName, craftingQuality, galaxy);
