beddaa0b54263c9e6748e400766e9d8a08b2c13b
Paul Willworth <ioscode@gmail.com>
2017-03-22 07:55:53 -0700

    Increase expire on galaxy sel cookie.

--- html/js/ghShared.js
+++ html/js/ghShared.js
@@ -1726,7 +1726,7 @@ function maskStats(typeSel, clearStats) {
 	}
 }
 function switchGalaxy(galaxy) {
-	setCookie("galaxy",galaxy,365);
+	setCookie("galaxy", galaxy, 999);
 	window.location.reload();
 }
 function toggleSection(aimg, id) {
